95bc5f757335b7b509ff552ab4bf0cfa1ee35ea2
goldkorec <270952@vut.cz>
2026-04-29 19:38:41 +0200

    uprava testbenche pro 4 switche

--- PWM_Breathing_LED/pwm.srcs/sim_1/new/tb_pwm_top.vhd
+++ PWM_Breathing_LED/pwm.srcs/sim_1/new/tb_pwm_top.vhd
@@ -11,22 +11,24 @@ architecture testbench of tb_pwm_top is
     signal s_clk     : std_logic;
     signal s_rst     : std_logic;
     signal s_en      : std_logic;
+    signal s_sw      : std_logic_vector(3 downto 0); 
     signal s_pwm_out : std_logic_vector(15 downto 0);
 
 begin
 
-    uut_pwm_top : entity work.pwm_top
+uut_pwm_top : entity work.pwm_top
         port map (
             clk     => s_clk,
             rst     => s_rst,
             en      => s_en,
+            sw      => s_sw,         
             pwm_out => s_pwm_out
         );
 
 
     p_clk_gen : process
     begin
-        while now < 10 ms loop
+        while now < 20 ms loop       
             s_clk <= '0';
             wait for c_CLK_100MHZ_PERIOD / 2;
             s_clk <= '1';
@@ -38,31 +40,50 @@ begin
 
     p_stimulus : process
     begin
-        report "Simulation start..." severity note;
+        
 
-       
+     
         s_en  <= '0';
         s_rst <= '0';
+        s_sw  <= "0000";
         wait for 50 ns;
 
        
         s_rst <= '1';
         wait for 100 ns;
 
-       
+     
         s_en  <= '1';
        
-       
-        wait for 2 ms;
+     
+        wait for 3 ms;
 
+   
+        report "Test rychlosti: SW 0" severity note;
+        s_sw <= "0001";
+        wait for 3 ms;
+
+   
+        report "Test rychlosti: SW 1" severity note;
+        s_sw <= "0010";
+        wait for 3 ms;
+
+        
+        report "Test rychlosti: SW 2" severity note;
+        s_sw <= "0100";
+        wait for 3 ms;
        
+
         s_rst <= '0';
         wait for 50 ns;
         s_rst <= '1';
        
-        wait for 1 ms;
+     
+        report "Test rychlosti: SW 3" severity note;
+        s_sw <= "1000";
+        wait for 3 ms;
 
-
+        report "Simulation end." severity note;
         wait;
     end process p_stimulus;
 
